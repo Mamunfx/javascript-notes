90c0af0ac8410d3aeb3d27b19ee5b7063afe06b1
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-08 16:39:12 +0000

    Variables in javascript !

---
+++ 02_basic.js
@@ -0,0 +1,18 @@
+//various types of VARIABLE :--
+
+var u = 69 ; //we dont use var anymore! 
+
+let a = 30;  // let is the general variable type!
+let b = 39;
+
+let c = a + b ;
+
+console.log( c );
+
+console.log(a+" + "+b + " = " + c);
+console.log(a+" - "+b + " = " + (a-b));
+console.log(a+" * "+b + " = " + (a*b));
+console.log(a+" / "+b + " = " + (a/b));
+
+const x = 230 ; // const is variable type which we cannot change in any cirmanstances!
+//let x = a+b ;    it will throw an error. 
