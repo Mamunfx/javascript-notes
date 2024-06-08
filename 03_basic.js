06cec13b18f326ad1b6f967c1e1819f73549444c
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-08 16:45:30 +0000

    DATA types in javascript !

---
+++ 03_basic.js
@@ -0,0 +1,14 @@
+
+// Various types of DATA TYPES in javascript :-- 
+
+let d = 10.2 ;
+let e = "EUro";
+let f = false ;
+let g 
+
+console.log(typeof a);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+
