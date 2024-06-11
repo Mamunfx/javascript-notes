6097c505384b27db77a07644e4398fe44fc14cf2
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-11 14:41:07 +0000

    If Else statement

--- 06_ifElse.js
+++ 06_ifElse.js
@@ -15,3 +15,18 @@ if (a>b) {
 
 let c = (a > b) ? "a is greater than b !"  : "b is greater than a !"
 console.log(c);
+
+
+
+// Else-if statement : Semilar than if else statement but we use else-if statement when we have more than one condition to deal.
+
+let x = 15;
+let y = 15;
+if (x>y) {
+    console.log("x is greater than y");
+} else if (x==y){
+    console.log("x is equal to y !");
+}
+else{
+    console.log("y is greater than x !");
+}
