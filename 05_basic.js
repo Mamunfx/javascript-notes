4dc558ebdeb3aba05769693e128cbad3334f7f0d
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-11 14:36:42 +0000

    Increment and decrement operator !

---
+++ 05_basic.js
@@ -0,0 +1,9 @@
+// Increment and  Decrement OPERATOR :--
+
+let n = 5;
+
+console.log(n + " " +  ++n + " " +  n++);   // ++n  is a pre increment and it will change value first then print
+console.log(n);                             // n++  is a post increment and it will change value in the second print not in the first time 
+
+console.log(n + " " +  --n + " " +  n--);   // ++n  is a pre increment and it will change value first then print
+console.log(n);                             // n++  is a post increment and it will change value in the second print not in the first time 
