c21f4d7af4bbbe27764331313595d4061ac08116
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-11 14:39:19 +0000

    IF_Else statement

---
+++ 06_ifElse.js
@@ -0,0 +1,17 @@
+// Conditional statement :
+
+// If-else statement : We use it when we need to compare and make a decision between some statement .
+
+let a = 20;
+let b = 12;
+
+if (a>b) {
+    console.log("a is greater than b !");
+} else {
+    console.log("b is greater than a !");
+}
+
+// Ternary If else expression :  Its the shortcut of previous statement .
+
+let c = (a > b) ? "a is greater than b !"  : "b is greater than a !"
+console.log(c);
