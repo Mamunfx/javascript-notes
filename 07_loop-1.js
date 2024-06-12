fcf6aee1a72645232ffc729a9b568006bb0d08a8
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-12 11:09:54 +0000

    LOOp !

--- 07_loop-1.js
+++ 07_loop-1.js
@@ -13,3 +13,25 @@ for (i = 0; i < 4; i++) {
 
 }
 console.log("Final Sum = " + sum);  // If we print outside the for loop it will return last result !! 
+
+
+
+/// While LOOP :--
+
+let Result = 0;
+let t = 0;
+while (t < 4) {
+    console.log("Result = " + Result + " t = " + t + " Result + t = " + (Result + t));
+    Result = Result + t;
+    t++;
+}
+console.log("Final Result = " + Result);
+
+
+/* 
+Use a for loop to iterate over an array.
+Use a for loop when you know the loop should execute n times.
+Use a while loop for reading a file into a variable.
+Use a while loop when asking for user input.
+Use a while loop when the increment value is nonstandard.
+*/
