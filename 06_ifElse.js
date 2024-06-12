2a776e596c10b7dab0e24650eb788bd0be65678f
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-12 11:05:41 +0000

    If-Else statement and switch !

--- 06_ifElse.js
+++ 06_ifElse.js
@@ -5,7 +5,7 @@
 let a = 20;
 let b = 12;
 
-if (a>b) {
+if (a > b) {
     console.log("a is greater than b !");
 } else {
     console.log("b is greater than a !");
@@ -13,7 +13,7 @@ if (a>b) {
 
 // Ternary If else expression :  Its the shortcut of previous statement .
 
-let c = (a > b) ? "a is greater than b !"  : "b is greater than a !"
+let c = (a > b) ? "a is greater than b !" : "b is greater than a !"
 console.log(c);
 
 
@@ -22,11 +22,52 @@ console.log(c);
 
 let x = 15;
 let y = 15;
-if (x>y) {
+if (x > y) {
     console.log("x is greater than y");
-} else if (x==y){
+} else if (x == y) {
     console.log("x is equal to y !");
 }
-else{
+else {
     console.log("y is greater than x !");
+}
+
+
+/*  Switch statement :  when we dont need to compare between many condition but have to see exact condition 
+                       and we have a long list of condition in that scanerio we use switch statement            */
+
+const day = 8;
+
+switch (day) {
+    case 0:
+        console.log('sunday');
+
+        break;
+    case 1:
+        console.log('monday');
+
+        break;
+    case 2:
+        console.log('tuesday');
+
+        break;
+    case 3:
+        console.log('wednesday');
+
+        break;
+    case 4:
+        console.log('thursday');
+
+        break;
+    case 5:
+        console.log('friday');
+
+        break;
+    case 6:
+        console.log('saturday');
+
+        break;
+
+    default:
+        console.log("Invalid day");
+
 }
