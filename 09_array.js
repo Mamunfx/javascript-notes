1462fe94991ea2aee0a28a4addc2a686d9fa130a
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-16 06:12:03 +0000

    Array part 2 !

--- 09_array.js
+++ 09_array.js
@@ -38,3 +38,25 @@ for(i=1; i < arr_4.length ; i++){
     }
 }
 console.log("Second smallest number is = " +secondSMallest);
+
+
+/// Array fill manual way :--
+
+const pack_1 = new Array(10);
+for (let i = 0; i < pack_1.length; i++) {
+    pack_1[i] = false;
+}
+console.log(pack_1);
+
+
+// Array fill easy method :--
+
+const pack_2 = new Array(10)
+pack_2.fill("euro");
+console.log(pack_2);
+
+const names = ["AMAR ","nam ","Mamun"];
+names[0] = "My ";
+names[1] = "is";
+names[2] = "Mamun"
+console.log(names);
