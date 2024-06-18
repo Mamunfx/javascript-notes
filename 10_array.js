f7fa2af01336e82e7a4e88ac1b385e2dce6a726d
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-18 03:52:59 +0000

    Array

--- 10_array.js
+++ 10_array.js
@@ -6,3 +6,13 @@ console.log(arr);
 function genten() {
     
 } ;
+
+
+// Array of object :--
+
+const stdn_info = [
+    {name : 'Mamun', age : '23'},         // When we need to store something which has more than one value in 
+    {name : 'Mamun', age : '23'},         //  itself then we use this method of array which store value as object inside array !
+    {name : 'Mamun', age : '23'},
+    {name : 'Mamun', age : '23'},
+] ;
