d2fa52e81f5fd63781e74d72021d84a257e7b4fd
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-18 03:51:54 +0000

    Array part 3

--- 10_array.js
+++ 10_array.js
@@ -1,1 +1,8 @@
-as
+// Array of everything :-- 
+
+const arr = [ 10 , 'ten' , true , genten , {ten : 10} , [10,10]];   // we can store every data type in array !
+console.log(arr);
+
+function genten() {
+    
+} ;
