05b6c44862386bf764bcb1766c8563c934f07bd0
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-21 17:47:21 +0000

    object

---
+++ 11_object.js
@@ -0,0 +1,17 @@
+// Object :--
+
+// Way of object define :--
+
+const person1 = {}
+person1.Names = "Md. Moonzoor Mamun Munna " ;
+person1.Age = 23 ;
+person1.Email = "md01993049420@gmail.com " ;
+
+
+const person2 ={
+    Names : "Shamima Akhter Srabonti " ,
+    Age : 23 ,
+    Email : "ShamimaAkhter@gmail.com"
+} 
+
+console.log(person1 , person2);
