cded1cc9c6807948b0a9fc8e565f66d9854e9ed4
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-22 06:02:48 +0000

    function

---
+++ 12_function.js
@@ -0,0 +1,14 @@
+/// FUnction :--
+
+let arr1 = [12, 40, 30, 99, 13];
+let arr2 = [17, 94, 83, 67, 25];
+
+
+// let max = arr1[0];
+
+// for (let i of arr1) {
+//   if (i > max) {
+//     max = i;
+//   }
+// }
+// console.log(max);
