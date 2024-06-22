2e05e34b972e2cbddac107fdcf349bac0a0a0857
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-22 06:03:22 +0000

    function

--- 12_function.js
+++ 12_function.js
@@ -12,3 +12,13 @@ let arr2 = [17, 94, 83, 67, 25];
 //   }
 // }
 // console.log(max);
+
+// let max2 = arr2[0];
+
+// for (let i of arr2) {
+//   if (i > max2) {
+//     max2 = i;
+//   }
+// }
+// console.log(max2);               // Instead repeating the same loop again again we can make a function .
+
