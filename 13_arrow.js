b37c138e5a2691436a33a1b4312cda29f7fbd3fe
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-06-23 15:20:32 +0000

    Function

---
+++ 13_arrow.js
@@ -0,0 +1,21 @@
+// Other way to define function :-- 
+
+// Arrow function :--
+
+let c = () => {
+    console.log('Hello world ! ');
+} ; 
+c(); 
+
+// Fat Arrow function :--
+
+let d = (a,b) => {
+    console.log(a+b);
+    return a+b ;
+};
+
+d(12,8);
+
+
+let e = (a,b) => a+b ;          // If our function cosist just one line we use this shortcut way !
+console.log(e(20,4));
